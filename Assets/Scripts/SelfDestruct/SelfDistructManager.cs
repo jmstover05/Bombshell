d69eabfaf9749f1c122c4ef12e2c3dee74a1ca79
jmstover05 <jmstover3315@gmail.com>
2026-04-16 18:20:05 -0400

    Created the self-destruct timer
    Created the self-destruct timer. If you want to disable the timer, you'll have to stop it from starting automatically in the SelfDestructManager code or call "StopSelfDestructTimer()" on the SelfDestructManager class. The SelfDestructManager has methods that still need to be implemented, such as what happens when the timer ends.

---
+++ Assets/Scripts/SelfDestruct/SelfDistructManager.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class SelfDestructManager: MonoBehaviour
+{
+    public SelfDestructHudManager HUD;
+    [Tooltip("The number of seconds the self-destruct timer starts with")]
+    public float initialTimeLeft = 120.0f;
+
+    private float currentTime = 0.0f;
+    private bool isSelfDestructing = false;
+
+    private void Start()
+    {
+        currentTime = initialTimeLeft;
+        StartSelfDestructTimer();
+    }
+
+    private void Update()
+    {
+        if (isSelfDestructing)
+        {
+            if (currentTime > 0.0f)
+            {
+                currentTime -= Time.deltaTime;
+                int minutes = Mathf.FloorToInt(currentTime) / 60;
+                int seconds = Mathf.FloorToInt(currentTime) % 60;
+                int miliseconds = Mathf.FloorToInt(currentTime * 100) % 100;
+                HUD.Timer.text = $"{minutes:00}:{seconds:00}:{miliseconds:00}";
+            }
+            else
+            {
+                SelfDestruct();
+            }
+        }
+    }
+    public float GetTimeLeft()
+    { 
+        return currentTime; 
+    }
+    public bool IsSelfDestructing()
+    {
+        return isSelfDestructing;
+    }
+    /// <summary>
+    /// Starts the self-distruct timer
+    /// Should be called when the player reaches the "end" of the level
+    /// </summary>
+    public void StartSelfDestructTimer()
+    {
+        isSelfDestructing = true;
+        HUD.background.SetActive(true);
+    }
+    /// <summary>
+    /// Stops the self-destruct timer
+    /// Should be called when the player returns to the beginning of the level after starting the self-destruct sequence
+    /// </summary>
+    public void StopSelfDestructTimer()
+    {
+        isSelfDestructing = false;
+        //play ending animation instead???
+        HUD.background.SetActive(false);
+    }
+    /// <summary>
+    /// Should end the game as a loss for the player by either setting the player's health to zero, or switching to
+    /// a unique scene that says the player failed to escape in time.
+    /// </summary>
+    private void SelfDestruct()
+    {
+        HUD.Timer.text = "00:00:00";
+        Debug.Log("AHH YOU EXPLODED");
+        return;
+    }
+
+}

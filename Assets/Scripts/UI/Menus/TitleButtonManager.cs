a2b2b81eafaabcff31f5c0cdd6c1bd38b686e8bf
Tony Assi <55763618+tonyassi913@users.noreply.github.com>
2026-04-21 15:02:05 -0400

    Added Quit button functionality

--- Assets/Scripts/UI/Menus/TitleButtonManager.cs
+++ Assets/Scripts/UI/Menus/TitleButtonManager.cs
@@ -4,15 +4,20 @@ using UnityEngine.SceneManagement;
 
 public class TitleButtonManager : MonoBehaviour
 {
-    void OnStartButtonClicked()
+    public void OnStartButtonClicked()
     {
         SceneManager.UnloadSceneAsync("Scenes/Title");
         SceneManager.LoadScene("Scenes/Game", LoadSceneMode.Single); // Assumes scenes are named Title and Game
     }
 
-    void OnCreditsButtonClicked()
+    public void OnCreditsButtonClicked()
     {
         SceneManager.UnloadSceneAsync("Scenes/Title");
         SceneManager.LoadScene("Scenes/Credits", LoadSceneMode.Single); // Assumes scenes are named Title and Credits
+    }
+
+    public void OnQuitButtonClicked()
+    {
+        Application.Quit();
     }
 }
